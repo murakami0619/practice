aac6cda1ae8a93e4d607c4518e2abeb08bfb60bf
murakami0619 <86032737+murakami0619@users.noreply.github.com>
2021-06-18 09:44:27 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,16 @@
+public avstract class TangibleAsset{
+  private String name;
+  private int price;
+  private String color;
+  
+  public TangibleAsset(String name,int price,String color){
+    this.name = name;
+    this.price = price;
+    this.color = color;
+  }
+  
+  public String getName(){ returm this.name;}
+  public int getPrice(){ returm this.price;}
+  public String getColor(){ returm this.color;}
+  
+}
